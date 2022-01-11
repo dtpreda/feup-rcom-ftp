aa154cc7a48de1889beeb2dcce7c7f4b1a2b37d6
David Preda <dtpreda30@gmail.com>
2022-01-11 18:54:14 +0000

    Add get_user_password

--- src/url/url.c
+++ src/url/url.c
@@ -9,6 +9,8 @@
 #define URL_CHARS_REGEX                                                        \
     "([[:alnum:]\\$\\.\\+\\*\\(\\),!'_-]+)|(%[[:xdigit:]]{2})"
 
+#define URL_FIELD_MAX 1024
+
 static const char url_regex[] =
         "^(ftp|FTP)://"
         "(((" URL_CHARS_REGEX ")*(:(" URL_CHARS_REGEX ")*)?@)?)"
@@ -20,7 +22,6 @@ static int validate_ftp_url(char *url) {
     const char *s = url;
     regex_t regex;
     regmatch_t pmatch[1];
-    regoff_t off, len;
 
     if (regcomp(&regex, url_regex, REG_EXTENDED)) {
         return ERROR;
@@ -36,6 +37,58 @@ static int validate_ftp_url(char *url) {
     if (url_len != match_len) {
         return ERROR;
     }
+
+    return SUCCESS;
+}
+
+static int get_user_password(char *url, char *user, char* password) {
+    if (strchr(url, '@') == NULL) {
+        return ERROR;
+    }
+
+    char user_pass_regex[] = "(" URL_CHARS_REGEX ")*:(" URL_CHARS_REGEX ")*@";
+    const char *s = url;
+    regex_t regex;
+    regmatch_t pmatch[1];
+
+    if (regcomp(&regex, user_pass_regex, REG_EXTENDED)) {
+        return ERROR;
+    }
+
+    if (regexec(&regex, s, 1, pmatch, 0)) {
+        return ERROR;
+    }
+
+    char split_regex[] = "(" URL_CHARS_REGEX ")*";
+    const char *user_password = s + pmatch[0].rm_so;
+
+    if (regcomp(&regex, split_regex, REG_EXTENDED)) {
+        return ERROR;
+    }
+
+    if (regexec(&regex, user_password, 1, pmatch, 0)) {
+        return ERROR;
+    }
+
+    if (pmatch[0].rm_eo - pmatch[0].rm_so >= URL_FIELD_MAX) {
+        return ERROR;
+    }
+
+    strncpy(user, user_password + pmatch[0].rm_so, pmatch[0].rm_eo - pmatch[0].rm_so);
+    user[pmatch[0].rm_eo] = '\0';
+
+    const char *pass = user_password + pmatch[0].rm_eo + 1;
+
+    if (regexec(&regex, pass, 1, pmatch, 0)) {
+        return ERROR;
+    }
+
+    if (pmatch[0].rm_eo - pmatch[0].rm_so >= URL_FIELD_MAX) {
+        return ERROR;
+    }
+
+    strncpy(password, pass + pmatch[0].rm_so, (pmatch[0].rm_eo - pmatch[0].rm_so));
+    password[pmatch[0].rm_eo] = '\0';
 
     return SUCCESS;
 }
