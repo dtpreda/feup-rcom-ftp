249a064b0f27a45c5cea296d3d3a34d7ec37eed2
David Preda <dtpreda30@gmail.com>
2022-01-11 19:42:09 +0000

    Add get_addr_port_path

--- src/url/url.c
+++ src/url/url.c
@@ -92,3 +92,79 @@ static int get_user_password(char *url, char *user, char* password) {
 
     return SUCCESS;
 }
+
+static int get_address_port_path(char *url, char* address, int* port, char* path) {
+    char addr_port_path_regex[] = "((" URL_CHARS_REGEX ")+(\\.(" URL_CHARS_REGEX ")+)+)((:[[:digit:]]{1,4})?)(/(" URL_CHARS_REGEX ")+)*[/]?";
+    const char *s = url;
+    regex_t regex;
+    regmatch_t pmatch[1];
+
+    if (regcomp(&regex, addr_port_path_regex, REG_EXTENDED)) {
+        return ERROR;
+    }
+
+    if (regexec(&regex, s, 1, pmatch, 0)) {
+        return ERROR;
+    }
+
+    const char *addr_port_path = s + pmatch[0].rm_so;
+    char addr_regex[] = "((" URL_CHARS_REGEX ")+(\\.(" URL_CHARS_REGEX ")+)+)";
+    
+    if (regcomp(&regex, addr_regex, REG_EXTENDED)) {
+        return ERROR;
+    }
+
+    if (regexec(&regex, addr_port_path, 1, pmatch, 0)) {
+        return ERROR;
+    }
+
+    if (pmatch[0].rm_eo - pmatch[0].rm_so >= URL_FIELD_MAX) {
+        return ERROR;
+    }
+
+    strncpy(address, addr_port_path + pmatch[0].rm_so, pmatch[0].rm_eo - pmatch[0].rm_so);
+    address[pmatch[0].rm_eo - pmatch[0].rm_so] = '\0';
+
+    char port_regex[] = ":[[:digit:]]{1,4}";
+    
+    if (regcomp(&regex, port_regex, REG_EXTENDED)) {
+        return ERROR;
+    }
+
+    if (regexec(&regex, addr_port_path, 1, pmatch, 0)) {
+        return ERROR;
+    }
+
+    if (pmatch[0].rm_eo - pmatch[0].rm_so >= URL_FIELD_MAX) {
+        return ERROR;
+    }
+
+    char port_str[URL_FIELD_MAX];
+    strncpy(port_str, addr_port_path + pmatch[0].rm_so + 1, pmatch[0].rm_eo - pmatch[0].rm_so - 1);
+    port_str[pmatch[0].rm_eo - pmatch[0].rm_so - 1] = '\0';
+
+    *port = atoi(port_str);
+
+    if (*port == 0) {
+        return ERROR;
+    }
+
+    char path_regex[] = "(/(" URL_CHARS_REGEX ")+)+[/]?";
+
+    if (regcomp(&regex, path_regex, REG_EXTENDED)) {
+        return ERROR;
+    }
+
+    if (regexec(&regex, addr_port_path, 1, pmatch, 0)) {
+        path = "";
+    } else {
+        if (pmatch[0].rm_eo - pmatch[0].rm_so >= URL_FIELD_MAX) {
+            return ERROR;
+        }
+
+        strncpy(path, addr_port_path + pmatch[0].rm_so + 1, pmatch[0].rm_eo - pmatch[0].rm_so - 2);
+        path[pmatch[0].rm_eo - pmatch[0].rm_so - 2] = '\0';
+    }
+
+    return SUCCESS;
+}
