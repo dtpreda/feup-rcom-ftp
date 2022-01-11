cd49ac9f9e91bbf25903585b4d0b866fbad9046e
David Preda <dtpreda30@gmail.com>
2022-01-11 17:35:45 +0000

    Add main.c

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <regex.h>
+
+#include "url/url.h"
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        printf("Wrong number of arguments\n");
+        return -1;
+    }
+
+    return 0;
+}
