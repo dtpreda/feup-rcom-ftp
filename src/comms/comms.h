7d0a28a75818e75e54cac7c41f97bee0af84199a
David Preda <dtpreda30@gmail.com>
2022-01-12 17:38:24 +0000

    Update comms header

--- src/comms/comms.h
+++ src/comms/comms.h
@@ -3,10 +3,10 @@
 
 int comm_set(char *addr, char* port);
 
-int comm_write();
+int comm_write(int socket_fd, char* msg);
 
-int comm_read();
+int comm_read(int socket_fd, char* buffer, int max_size);
 
-int comm_kill();
+int comm_kill(int socket_fd);
 
 #endif /* COMMS */
