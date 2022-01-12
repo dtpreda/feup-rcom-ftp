6f8a24c0f103abc66fded2b422430a1c8982ae6d
David Preda <dtpreda30@gmail.com>
2022-01-12 18:17:33 +0000

    Update includes on ftp

--- src/ftp/ftp.c
+++ src/ftp/ftp.c
@@ -1,5 +1,7 @@
 #include "../common/common.h"
-#include "../comms/comms.h"
+#include "comms/comms.h"
+#include "command/command.h"
+#include "reply/reply.h"
 #include "ftp.h"
 
 int login(char *user, char *password) {
