27a1a81106ceb5be4a2e05174611859ebaafb3e7
David Preda <dtpreda30@gmail.com>
2022-01-16 23:12:33 +0000

    Update user command

--- src/ftp/command/command.c
+++ src/ftp/command/command.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "../../common/common.h"
@@ -8,13 +8,13 @@
 
 #define MAX_LEN 1024
 
-int user(int socket_fd, char* username) {
-    char command[MAX_LEN] = "user ";
-    strncat(command, username, MAX_LEN - 5);
+int user(int socket_fd, char *username) {
+  char command[MAX_LEN] = "user ";
+  strncat(command, username, MAX_LEN - 5);
 
-    if (comm_write(socket_fd, command) != SUCCESS) {
-        return ERROR;
-    }
+  if (comm_write(socket_fd, command) != SUCCESS) {
+    return ERROR;
+  }
 
-    return SUCCESS;
+  return SUCCESS;
 }
--- src/main.c
+++ src/main.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <regex.h>
 
+#include "common/common.h"
+#include "ftp/comms/comms.h"
+#include "ftp/command/command.h"
 #include "url/url.h"
 
 int main(int argc, char* argv[]) {
@@ -9,6 +12,21 @@ int main(int argc, char* argv[]) {
         printf("Wrong number of arguments\n");
         return -1;
     }
+
+    struct url_fields urlf;
+    if (process_url(argv[1], &urlf) == ERROR) {
+        return ERROR;
+    }
+
+    printf("Port: %s\n", urlf.port);
+
+    int fd = comm_set(urlf.addr, urlf.port);
+
+    if (user(fd, urlf.user) != SUCCESS) {
+        printf("Error\n");
+    }
+
+    comm_kill(fd);
 
     return 0;
 }
