c0f30413b2bfe40a1e11762af554a9772572f31f
David Preda <dtpreda30@gmail.com>
2022-01-17 15:31:45 +0000

    Add login function

--- src/ftp/ftp.c
+++ src/ftp/ftp.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "../common/common.h"
 #include "comms/comms.h"
 #include "command/command.h"
@@ -5,5 +7,39 @@
 #include "ftp.h"
 
 int login(int socket_fd, char *username, char *password) {
-    return SUCCESS;
+    if (send_command(socket_fd, user, username)) {
+        printf("Unable to send user command\n");
+        return ERROR;
+    }
+
+    int reply_code;
+    if ((reply_code = process_reply(socket_fd, parse_user, NULL)) == ERROR) {
+        printf("Couldn't receive a proper answer from the server\n");
+        return ERROR;
+    }
+
+    switch (reply_code)
+    {
+    case 230:
+        return SUCCESS;
+    case 331:
+        if (send_command(socket_fd, pass, password) == ERROR) {
+            printf("Unable to send pass command\n");
+            return ERROR;
+        }
+
+        if ((reply_code = process_reply(socket_fd, parse_password, NULL)) == ERROR) {
+            printf("Couldn't receive a proper answer from the server\n");
+            return ERROR;
+        }
+
+        if (reply_code == 202 || reply_code == 230) {
+            return SUCCESS;
+        }
+
+        return ERROR;
+    case 332:
+    default:
+        return ERROR;
+    }
 }
