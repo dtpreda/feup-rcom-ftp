862879bfb94a7af093b944783412312b40d49941
David Preda <dtpreda30@gmail.com>
2022-01-17 11:23:16 +0000

    Reorder functions

--- src/ftp/reply/reply.c
+++ src/ftp/reply/reply.c
@@ -10,27 +10,6 @@
 
 static enum rsm {WHITESPACE, MISC, END};
 
-int process_reply(int socket_fd, int (*parser)(char *reply, int len, char *ret), char *ret) {
-    int partial_len = 0, total_len = 0;
-    char partial[MAX_PARTIAL_MSG_SIZE];
-    char reply[MAX_MSG_SIZE];
-
-    while ((partial_len = comm_read(socket_fd, partial, MAX_PARTIAL_MSG_SIZE)) != ERROR) {
-        if (total_len + partial_len >= MAX_MSG_SIZE) {
-            return ERROR;
-        }
-
-        strncat(reply, partial, MAX_MSG_SIZE - total_len);
-        total_len += partial_len;
-
-        if (is_done(reply, total_len)) {
-            return parser(reply, total_len, ret);
-        }
-    }
-
-    return ERROR;
-}
-
 static int is_done(char *reply, int len) {
     if (len == 3) {
         return atoi(reply) != 0;
@@ -68,6 +47,28 @@ static int is_done(char *reply, int len) {
             return _rsm == END;
         }
     }
+    return ERROR;
+}
+
+int process_reply(int socket_fd, int (*parser)(char *reply, int len, char *ret), char *ret) {
+    int partial_len = 0, total_len = 0;
+    char partial[MAX_PARTIAL_MSG_SIZE];
+    char reply[MAX_MSG_SIZE];
+
+    while ((partial_len = comm_read(socket_fd, partial, MAX_PARTIAL_MSG_SIZE)) != ERROR) {
+        if (total_len + partial_len >= MAX_MSG_SIZE) {
+            return ERROR;
+        }
+
+        strncat(reply, partial, MAX_MSG_SIZE - total_len);
+        total_len += partial_len;
+
+        if (is_done(reply, total_len)) {
+            printf("%s\n", reply);
+            return parser(reply, total_len, ret);
+        }
+    }
+
     return ERROR;
 }
 
