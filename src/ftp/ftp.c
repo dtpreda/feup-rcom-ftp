c074f9a93a33d59cdd25d570c61e8d0d1d8e4a3f
David Preda <dtpreda30@gmail.com>
2022-01-18 17:53:32 +0000

    Rename variables in ftp module

--- src/ftp/ftp.c
+++ src/ftp/ftp.c
@@ -9,30 +9,30 @@
 #include "ftp.h"
 
 int set_up_cmd(char *addr, char* port) {
-    int fd;
+    int cmd_fd;
    
-   if ((fd  = comm_set(addr, port)) == ERROR) {
+   if ((cmd_fd  = comm_set(addr, port)) == ERROR) {
        return ERROR;
    }
 
-    if (process_reply(fd, parse_connect, NULL, 0) == ERROR) {
+    if (process_reply(cmd_fd, parse_connect, NULL, 0) == ERROR) {
         printf("Could not connect\n");
         return ERROR;
     }
 
     printf("Connection established\n");
 
-    return fd;
+    return cmd_fd;
 }
 
-int login(int socket_fd, char *username, char *password) {
-    if (send_command(socket_fd, user, username)) {
+int login(int cmd_fd, char *username, char *password) {
+    if (send_command(cmd_fd, user, username)) {
         printf("Unable to send user command\n");
         return ERROR;
     }
 
     int reply_code;
-    if ((reply_code = process_reply(socket_fd, parse_user, NULL, 0)) == ERROR) {
+    if ((reply_code = process_reply(cmd_fd, parse_user, NULL, 0)) == ERROR) {
         printf("Couldn't receive a proper answer from the server\n");
         return ERROR;
     }
@@ -42,12 +42,12 @@ int login(int socket_fd, char *username, char *password) {
     case 230:
         return SUCCESS;
     case 331:
-        if (send_command(socket_fd, pass, password) == ERROR) {
+        if (send_command(cmd_fd, pass, password) == ERROR) {
             printf("Unable to send pass command\n");
             return ERROR;
         }
 
-        if ((reply_code = process_reply(socket_fd, parse_password, NULL, 0)) == ERROR) {
+        if ((reply_code = process_reply(cmd_fd, parse_password, NULL, 0)) == ERROR) {
             printf("Couldn't receive a proper answer from the server\n");
             return ERROR;
         }
@@ -63,14 +63,14 @@ int login(int socket_fd, char *username, char *password) {
     }
 }
 
-int set_pasv(int socket_fd, char* port) {
-    if (send_command(socket_fd, pasv, NULL) == ERROR) {
+int set_pasv(int cmd_fd, char* port) {
+    if (send_command(cmd_fd, pasv, NULL) == ERROR) {
         printf("Unable to send pasv command\n");
         return ERROR;
     }
 
     int reply_code;
-    if ((reply_code = process_reply(socket_fd, parse_pasv, port, 10)) == ERROR)
+    if ((reply_code = process_reply(cmd_fd, parse_pasv, port, 10)) == ERROR)
     {
         printf("Couldn't receive a proper answer from the server\n");
         return ERROR;
@@ -79,15 +79,15 @@ int set_pasv(int socket_fd, char* port) {
     return SUCCESS;
 }
 
-int is_file(int socket_fd, char* path) {
-    if (send_command(socket_fd, stat, path) == ERROR) {
+int is_file(int cmd_fd, char* path) {
+    if (send_command(cmd_fd, stat, path) == ERROR) {
         printf("Unable to send pasv command\n");
         return ERROR;
     }
 
     int reply_code;
     char ans[5];
-    if ((reply_code = process_reply(socket_fd, parse_stat, ans, 5)) == ERROR)
+    if ((reply_code = process_reply(cmd_fd, parse_stat, ans, 5)) == ERROR)
     {
         printf("Couldn't receive a proper answer from the server\n");
         return ERROR;
