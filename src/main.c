768313f6b9e0954b11bf8d5978ee21ed8e142099
David Preda <dtpreda30@gmail.com>
2022-01-18 18:15:04 +0000

    Add main

--- src/main.c
+++ src/main.c
@@ -1,11 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <regex.h>
+#include <string.h>
 
 #include "common/common.h"
 #include "ftp/comms/comms.h"
-#include "ftp/command/command.h"
+#include "ftp/ftp.h"
 #include "url/url.h"
+#include "file/file.h"
+
+#define MAX_FILE_SIZE 256000
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
@@ -18,15 +22,74 @@ int main(int argc, char* argv[]) {
         return ERROR;
     }
 
-    printf("Port: %s\n", urlf.port);
+    int cmd_fd;
 
-    int fd = comm_set(urlf.addr, urlf.port);
+    atexit(close_ftp);
 
-    if (user(fd, urlf.user) != SUCCESS) {
-        printf("Error\n");
+    printf("Trying to connect to requested server...\n");
+
+    if ((cmd_fd = set_up_cmd(urlf.addr, urlf.port)) == ERROR) {
+        printf("Unable to connect\n");
+        return ERROR;
     }
 
-    comm_kill(fd);
+    printf("Succesfully connected to server\n");
+    printf("Logging in...\n");
 
-    return 0;
+    if (login(cmd_fd,urlf.user, urlf.password) == ERROR) {
+        printf("Unable to login\n");
+        return ERROR;
+    }
+
+    printf("Logged in succesfully.\n");
+
+    printf("Verifying if requested resource is a file...\n");
+    if (is_file(cmd_fd, urlf.path) == ERROR)
+    {
+        printf("Resource requested is not a file\n");
+        return ERROR;
+    }
+
+    printf("Requested resource is a file.\n");
+
+    printf("Enabling passive mode...\n");
+
+    char port[10];
+    if (set_pasv(cmd_fd, port) == ERROR) {
+        printf("Unable to set passive mode\n");
+        return ERROR;
+    }
+
+    printf("Passive mode enabled.\n");
+
+    printf("Connecting to file transfer port...\n");
+
+    int dl_fd;
+    if ((dl_fd = set_up_download(cmd_fd, urlf.addr, port, urlf.path)) == ERROR)
+    {
+        printf("Unable to connect to file transfer port\n");
+        return ERROR;
+    }
+
+    printf("Connected.\n");
+    printf("Initiating transfer...\n");
+
+    char data[MAX_FILE_SIZE];
+    if (download(dl_fd, data, MAX_FILE_SIZE) == ERROR)
+    {
+        printf("Unable to download requeste resource\n");
+        return ERROR;
+    }
+
+    printf("Transfer complete.\n");
+
+    if (save_file(data, MAX_FILE_SIZE, urlf.filename, URL_FIELD_MAX))
+    {
+        printf("Unable to save file\n");
+        return ERROR;
+    }
+
+    printf("Requested resource saved as %s\n", urlf.filename);
+
+    return SUCCESS;
 }
